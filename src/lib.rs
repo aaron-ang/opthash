8137907e80798dbdcd1278ec12548cc78bea8b87
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2026-03-18 00:03:22 -0400

    increase simd width

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,18 @@ mod funnel;
 
 pub use elastic::ElasticHashMap;
 pub use funnel::FunnelHashMap;
+
+#[doc(hidden)]
+pub mod bench_support {
+    #[inline]
+    pub fn control_match_free_group(chunk: &[u8]) -> u32 {
+        <[u8] as crate::common::Controls>::match_free_group(chunk)
+    }
+
+    #[inline]
+    pub fn control_match_fingerprint_group(chunk: &[u8], target: u8) -> u32 {
+        <[u8] as crate::common::Controls>::match_fingerprint_group(chunk, target)
+    }
+
+    pub const CTRL_EMPTY: u8 = crate::common::CTRL_EMPTY;
+}
