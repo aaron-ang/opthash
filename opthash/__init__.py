3d9e557b4bdfcb52be4f70c1a662241980e2a717
Aaron Ang <aaron.angyd@gmail.com>
2026-05-09 15:36:57 -0400

    feat(python): add type stubs and consolidate package layout

---
+++ opthash/__init__.py
@@ -0,0 +1,30 @@
+from collections.abc import ItemsView, KeysView, MutableMapping, ValuesView
+
+from .opthash import (
+    ElasticHashMap,
+    ElasticOptions,
+    FunnelHashMap,
+    FunnelOptions,
+    elastic_items,
+    elastic_keys,
+    elastic_values,
+    funnel_items,
+    funnel_keys,
+    funnel_values,
+)
+
+MutableMapping.register(ElasticHashMap)
+MutableMapping.register(FunnelHashMap)
+KeysView.register(elastic_keys)
+KeysView.register(funnel_keys)
+ValuesView.register(elastic_values)
+ValuesView.register(funnel_values)
+ItemsView.register(elastic_items)
+ItemsView.register(funnel_items)
+
+__all__ = [
+    "ElasticHashMap",
+    "ElasticOptions",
+    "FunnelHashMap",
+    "FunnelOptions",
+]
